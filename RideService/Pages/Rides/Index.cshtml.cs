f276b50425e089a60c058c51bc887b2a8b422715
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-04-24 13:26:32 +0200

    working on rides index

--- RideService/Pages/Rides/Index.cshtml.cs
+++ RideService/Pages/Rides/Index.cshtml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RideService.Models;
 
 namespace RideService.Pages.Rides
 {
     public class IndexModel : PageModel
     {
+        public List<Ride> Rides { get; set; }
         public void OnGet()
         {
 
