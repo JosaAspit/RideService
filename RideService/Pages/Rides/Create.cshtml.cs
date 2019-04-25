6af6e0242c59403629b6ac56c2c903507fbb1f6f
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-04-25 13:16:11 +0200

    initial work on create ride page

--- RideService/Pages/Rides/Create.cshtml.cs
+++ RideService/Pages/Rides/Create.cshtml.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RideService.Models;
 
 namespace RideService.Pages.Rides
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Description { get; set; }
+        [BindProperty]
+        public Status Status { get; set; }
+        [BindProperty]
+        public int CategoryId { get; set; }
+
+        public SelectList CategoryIds { get; set; }
         public void OnGet()
         {
 
