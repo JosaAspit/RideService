28ba072f934a2fddead6fb520192740c228244f3
Woody <bastholm.casper@gmail.com>
2019-04-25 14:37:58 +0200

    Now you can see all the reports for a ride

---
+++ RideService/Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RideService.Logic;
+using RideService.Models;
+
+namespace RideService.Pages.Reports
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+        public Ride Ride { get; set; }
+
+        public void OnGet()
+        {
+            RideRepository rideRepository = new RideRepository();
+
+            Ride = rideRepository.GetRide(Id);
+        }
+    }
+}
